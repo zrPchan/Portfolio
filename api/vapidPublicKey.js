58c63a94f27fb04061e30af3e9884b5d005abf57
0Pchan <4uth9gotegote@gmail.com>
2025-11-06 02:29:13 +0900

    VAPID公開鍵の取得方法を更新: プレーンテキスト形式で返すように変更し、204 No Contentを適切に処理

--- api/vapidPublicKey.js
+++ api/vapidPublicKey.js
@@ -1,6 +1,12 @@
 const { VAPID_PUBLIC_KEY } = require('./_lib');
 
+// Return the raw VAPID public key as text so the client can use it directly
 module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(JSON.stringify({ publicKey: VAPID_PUBLIC_KEY || '' }));
+  if (VAPID_PUBLIC_KEY) {
+    res.setHeader('Content-Type', 'text/plain');
+    return res.status(200).send(String(VAPID_PUBLIC_KEY));
+  }
+  // If not configured, return 204 No Content to indicate empty
+  res.setHeader('Content-Type', 'text/plain');
+  return res.status(204).send('');
 };
